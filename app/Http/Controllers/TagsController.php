09621dfa13361a93d8c7f49cd342d53b66031da8
John LeVan <jlevan@gmail.com>
2016-04-07 15:22:36 -0500

    Adding DocBlocks

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -11,15 +11,24 @@ use Input, Validator, Session, Redirect;
 
 class TagsController extends Controller {
 
+  /**
+   * @return $this
+   */
   public function index() {
     $tags = Tags::all();
     return view('tags.index')->with('tags', $tags);
   }
 
+  /**
+   * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+   */
   public function create() {
     return view('tags.create');
   }
 
+  /**
+   * @return mixed
+   */
   public function store() {
     $rules = array(
       'name'       => 'required'
@@ -45,6 +54,10 @@ class TagsController extends Controller {
     }
   }
 
+  /**
+   * @param $id
+   * @return mixed
+   */
   public function update($id) {
     $rules = array(
       'name'       => 'required'
@@ -70,18 +83,30 @@ class TagsController extends Controller {
     }
   }
 
+  /**
+   * @param $id
+   * @return mixed
+   */
   public function destroy($id) {
     $tag = Tags::find($id);
     $tag->delete();
     return Redirect::to('tags');
   }
 
+  /**
+   * @param $id
+   * @return $this
+   */
   public function show($id) {
     $tag = Tags::find($id);
     return view('tags.show')
       ->with('tag', $tag);
   }
 
+  /**
+   * @param $id
+   * @return $this
+   */
   public function edit($id) {
     $tag = Tags::find($id);
     return view('tags.edit')
