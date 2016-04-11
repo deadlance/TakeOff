abe235d26267d799121f88c278efc8754afad67e
John LeVan <jlevan@gmail.com>
2016-04-11 15:07:39 -0500

    Finishing up the Building Materials section of the app. - Finished all the UI / UX interactions for Building Materials

--- app/Http/Controllers/BuildingMaterialsController.php
+++ app/Http/Controllers/BuildingMaterialsController.php
@@ -6,56 +6,150 @@ use Illuminate\Http\Request;
 use \App\BuildingMaterial;
 
 use App\Http\Requests;
+use Input, Validator, Session, Redirect;
 
-class BuildingMaterialsController extends Controller
-{
-    public function index() {
+class BuildingMaterialsController extends Controller {
+  /**
+   * @return mixed
+   */
+  public function index() {
+    return BuildingMaterial::orderBy('name')->with('unit_of_measure')->with('tags')->get();
+    //Tags::orderBy('name')->get()
+  }
 
-      /*
-      $b = new BUildingMaterial;
-      $b->name = 'example';
-      $b->slug = 'example-slug';
-      $b->description = 'example description';
-      $b->unit_of_measure_id = 6;
-      $b->save();
-      */
+  /**
+   * @param $id
+   */
+  public function destroy($id) {
+    $buildingMaterial = BuildingMaterial::find($id);
+    $buildingMaterial->delete();
+  }
 
-      //$b = BuildingMaterial::find(8);
-      //$b->tags()->attach([16,17,18]);
+  /**
+   * @return BuildingMaterial
+   */
+  public function store() {
+    $rules = array(
+      'name'       => 'required'
+    );
+    $validator = Validator::make(Input::all(), $rules);
+    if ($validator->fails()) {
+      return $buildingMaterial;
+    } else {
+      $buildingMaterial = new BuildingMaterial;
+      $buildingMaterial->name = Input::get('name');
+      $buildingMaterial->description = Input::get('description');
+      $buildingMaterial->unit_of_measure_id = Input::get('unit_of_measure');
 
-      //return "<pre>" .json_encode(BuildingMaterial::with('unit_of_measure')->with('tags')->get(), JSON_PRETTY_PRINT) . "</pre>";
-      return BuildingMaterial::with('unit_of_measure')->with('tags')->get();
+      $buildingMaterial->slug = preg_replace("/[^a-z0-9\s\-]/i", "", $buildingMaterial->name . '_' . $buildingMaterial->unit_of_measure_id);
+      $buildingMaterial->slug = preg_replace("/\s\s+/", " ", $buildingMaterial->slug);
+      $buildingMaterial->slug = preg_replace("/\s/", "-", $buildingMaterial->slug);
+      $buildingMaterial->slug = preg_replace("/\-\-+/", "-", $buildingMaterial->slug);
+      $buildingMaterial->slug = trim($buildingMaterial->slug, "-");
+      $buildingMaterial->slug = strtolower($buildingMaterial->slug);
+
+      $buildingMaterial->save();
+      return $buildingMaterial;
     }
+  }
 
-    public function getTags($id) {
-      $buildingMaterial = BuildingMaterial::findOrFail($id)->with('tags')->where('id', $id)->get();
-      //return "<pre>" .json_encode($buildingMaterial[0]['tags'], JSON_PRETTY_PRINT) . "</pre>";
-      return $buildingMaterial[0]['tags'];
-    }
+  /**
+   * @param $id
+   * @return mixed
+   */
+  public function update($id) {
+    $rules = array(
+      'name'       => 'required'
+    );
+    $validator = Validator::make(Input::all(), $rules);
+    if ($validator->fails()) {
+      return $buildingMaterial;
+    } else {
+      $buildingMaterial = BuildingMaterial::find($id);
+      $buildingMaterial->name = Input::get('name');
+      $buildingMaterial->description = Input::get('description');
+      $buildingMaterial->unit_of_measure_id = Input::get('unit_of_measure');
 
-    public function addTag($buildingMaterialID, $tagID) {
-      // Here we need to add the tagID to the buildingMaterialID
-      $buildingMaterial = BuildingMaterial::find($buildingMaterialID);
-      if($buildingMaterial->tags()->attach($tagID)) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
-    }
+      $buildingMaterial->slug = preg_replace("/[^a-z0-9\s\-]/i", "", $buildingMaterial->name . '_' . $buildingMaterial->unit_of_measure_id);
+      $buildingMaterial->slug = preg_replace("/\s\s+/", " ", $buildingMaterial->slug);
+      $buildingMaterial->slug = preg_replace("/\s/", "-", $buildingMaterial->slug);
+      $buildingMaterial->slug = preg_replace("/\-\-+/", "-", $buildingMaterial->slug);
+      $buildingMaterial->slug = trim($buildingMaterial->slug, "-");
+      $buildingMaterial->slug = strtolower($buildingMaterial->slug);
 
-    public function removeTag($buildingMaterialID, $tagID) {
-      // Here we need to add the tagID to the buildingMaterialID
-      $buildingMaterial = BuildingMaterial::find($buildingMaterialID);
-      if($buildingMaterial->tags()->detach($tagID)) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
-    }
+      $buildingMaterial->save();
 
-    public function webIndex() {
-      return view('building_materials.index');
+      return $buildingMaterial;
     }
+  }
+
+  /**
+   * @param $id
+   * @return \Illuminate\Database\Eloquent\Collection|static[]
+   */
+  public function show($id) {
+    $buildingMaterial = BuildingMaterial::with('unit_of_measure')->with('tags')->where('id', $id)->get();
+    return $buildingMaterial;
+  }
+
+  /**
+   *
+   */
+  public function create() {
+    // web form for creating a building material. I'm not sure we'll need this.
+  }
+
+  /**
+   *
+   */
+  public function edit() {
+    // web form for updating a building material. I'm not sure we'll need this.
+  }
+
+
+  /**
+   * @param $id
+   * @return mixed
+   */
+  public function getTags($id) {
+    $buildingMaterial = BuildingMaterial::findOrFail($id)->with('tags')->where('id', $id)->get();
+    return $buildingMaterial[0]['tags'];
+  }
+
+  /**
+   * @param $buildingMaterialID
+   * @param $tagID
+   * @return int
+   */
+  public function addTag($buildingMaterialID, $tagID) {
+    $buildingMaterial = BuildingMaterial::find($buildingMaterialID);
+    if ($buildingMaterial->tags()->attach($tagID)) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  /**
+   * @param $buildingMaterialID
+   * @param $tagID
+   * @return int
+   */
+  public function removeTag($buildingMaterialID, $tagID) {
+    $buildingMaterial = BuildingMaterial::find($buildingMaterialID);
+    if ($buildingMaterial->tags()->detach($tagID)) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  /**
+   * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+   */
+  public function webIndex() {
+    return view('building_materials.index');
+  }
 }
--- resources/views/building_materials/index.blade.php
+++ resources/views/building_materials/index.blade.php
@@ -8,16 +8,27 @@
         }
     </style>
     <script>
+
+        jQuery.each(['put', 'delete'], function(i, method) {
+           jQuery[method] = function(url, data, callback, type) {
+               if(jQuery.isFunction(data)) {
+                   type = type || callback;
+                   callback = data;
+                   data = undefined;
+               }
+               return jQuery.ajax({
+                   url: url,
+                   type: method,
+                   dataType: type,
+                   data: data,
+                   success: callback
+               });
+           };
+        });
+
         var allTags = '';
+        var unitsOfMeasure = '';
         $(document).ready(function () {
-            $.ajax({
-                url: '/api/building_materials',
-                type: "get",
-                dataType: 'json',
-                success: function (data) {
-                    loadBuildingMaterials(data);
-                }
-            });
             $.ajax({
                 url: '/api/tags',
                 type: "get",
@@ -26,22 +37,72 @@
                     allTags = data;
                 }
             });
+
+            $.ajax({
+                url: '/api/unit_of_measure',
+                type: "get",
+                dataType: 'json',
+                success: function (data) {
+                    unitsOfMeasure = data;
+                    data.forEach(function(d) {
+                        $("#new_unit_of_measure").append("<option value='" + d['id'] + "'>" + d['name'] + "</option>");
+                    });
+                },
+                complete: function(data) {
+
+                    initBM();
+                }
+            });
         });
+
+        function initBM() {
+            $.ajax({
+                url: '/api/building_materials',
+                type: "get",
+                dataType: 'json',
+                success: function (data) {
+                    loadBuildingMaterials(data);
+                }
+            });
+        }
 
         function loadBuildingMaterials(buildingMaterials) {
             var fullList = "";
+            $('#building_materials_list').html('');
             buildingMaterials.forEach(function (BM) {
                 fullList = fullList + "<div class='panel' data-toggle='tooltip' data-placement='bottom' title='" + BM['description'] + "'>";
                 fullList = fullList + "<div class='panel-body'>";
-                fullList = fullList + "<div class='col-lg-1'>" + BM['id'] + "</div>";
-                fullList = fullList + "<div class='col-lg-2'>" + BM['name'] + "</div>";
-                fullList = fullList + "<div class='col-lg-4'>" + BM['description'] + "</div>";
-                fullList = fullList + "<div class='col-lg-2'>" + BM['unit_of_measure']['name'] + "</div>";
+
+                // start of form
+                fullList = fullList + '<form action="" method="" onSubmit="return false;" class="updateForm" id="updateForm_' + BM['id'] + '">';
+                fullList = fullList + '{!! Form::token() !!}';
+                fullList = fullList + "<div class='col-lg-1'><div class='form-group'><input type='hidden' name='id' value='" + BM['id'] + "' />" + BM['id'] + "</div></div>";
+                fullList = fullList + "<div class='col-lg-2'><div class='form-group'><input type='text' class='form-control' name='name' value='" + BM['name'] + "' /></div></div>";
+                fullList = fullList + "<div class='col-lg-4'><div class='form-group'><input type='text' class='form-control' name='description' value='" + BM['description'] + "' /></div></div>";;
+                fullList = fullList + "<div class='col-lg-2'>";
+                fullList = fullList + "<div class='form-group'>";
+                fullList = fullList + "<select class='form-control' name='unit_of_measure' id='unitSelector_" + BM['id'] + "'>";
+                //fullList = fullList + BM['unit_of_measure']['name'];
+                unitsOfMeasure.forEach(function (u) {
+                    fullList = fullList + "<option value='" + u['id'] + "'";
+                    if(BM['unit_of_measure']['id'] == u['id']) {
+                        fullList = fullList + " selected";
+                    }
+                    fullList = fullList + ">" + u['name'] + "</option>";
+                })
+                fullList = fullList + "</select>";
+                fullList = fullList + "</div>";
+                fullList = fullList + "</div>";
                 fullList = fullList + "<div class='col-lg-3'><div class='btn-group'>" +
                         "<button type='button' class='btn btn-warning' onClick='showTags(" + BM['id'] + ")'>Tags</button>" +
-                        "<button type='button' class='btn btn-success'>Edit</button>" +
-                        "<button type='button' class='btn btn-danger'>Delete</button>" +
+                        "<button type='button' class='btn btn-success' onClick='updateBM(" + BM['id'] + ")'>Save</button>" +
+                        "<button type='button' class='btn btn-danger' onClick='deleteBM(" + BM['id'] + ")'>Delete</button>" +
                         "</div></div>";
+
+                // end of form
+                fullList = fullList + '</form>';
+
+
                 fullList = fullList + "</div>";
                 fullList = fullList + "</div>";
             });
@@ -49,6 +110,77 @@
             //$('#building_materials_list').append(buildingMaterials[0]['id']);
             $('#building_materials_list').append(fullList);
 
+        }
+
+        function deleteBM(BMID) {
+            var form = $("#updateForm_" + BMID);
+            var csrf = form.find('input[name="_token"]').val();
+            $.ajax({
+                url: '/api/building_materials/' + BMID,
+                type: 'DELETE',
+                data: { id: BMID, _token: csrf },
+                dataType: 'JSON',
+                complete: function (data) {
+                    initBM();
+                }
+            });
+        }
+
+        function updateBM(BMID) {
+            var form = $("#updateForm_" + BMID);
+            var BMname = form.find('input[name="name"]').val();
+            var BMdescription = form.find('input[name="description"]').val();
+            var BMunit_of_measure = $("#unitSelector_" + BMID).val()
+            var csrf = form.find('input[name="_token"]').val();
+
+            if(BMname == '') {
+                alert('Name must be filled in.');
+                exit;
+            }
+
+            if(BMunit_of_measure == '') {
+                alert('You must choose a Unit of Measure.');
+                exit;
+            }
+
+            $.ajax({
+                url: '/api/building_materials/' + BMID,
+                type: 'PUT',
+                data: { _method: 'PUT', id: BMID, name: BMname, description: BMdescription, unit_of_measure: BMunit_of_measure, _token: csrf },
+                dataType: 'JSON',
+                complete: function (data) {
+                    initBM();
+                }
+            });
+        }
+
+        function createBM() {
+            var form = $("#newBM");
+            var BMname = form.find('input[name="new_name"]').val();
+            var BMdescription = form.find('input[name="new_description"]').val();
+            var BMunit_of_measure = $("#new_unit_of_measure").val()
+            var csrf = form.find('input[name="_token"]').val();
+
+            if(BMname == '') {
+                alert('Name must be filled in.');
+                exit;
+            }
+
+            if(BMunit_of_measure == '') {
+                alert('You must choose a Unit of Measure.');
+                exit;
+            }
+
+            $.ajax({
+                url: '/api/building_materials',
+                type: 'POST',
+                data: { name: BMname, description: BMdescription, unit_of_measure: BMunit_of_measure, _token: csrf },
+                dataType: 'JSON',
+                complete: function (data) {
+                    initBM();
+                    form.find("input[type=text], select").val("");
+                }
+            });
         }
 
         function showTags(BMID) {
@@ -115,9 +247,31 @@
 @endsection
 
 @section('content')
+
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
+                <div class="panel">
+                    <form id="newBM" onSubmit="return false;">
+                    <div class="panel-body">
+                        <div class="col-lg-1"><div class='form-group'><label>ID</label>{!! Form::token() !!}</div></div>
+                        <div class="col-lg-2"><div class='form-group'><label for="new_name">Name</label><input type="text" class="form-control" name="new_name" id="new_name" /></div></div>
+                        <div class="col-lg-4"><div class='form-group'><label for="new_description">Description</label><input type="text" class="form-control" name="new_description" id="new_description" /></div></div>
+                        <div class="col-lg-2">
+                            <div class='form-group'>
+                                <label for="unit_of_measure">Unit of Measure</label>
+
+                                <select class='form-control' name='unit_of_measure' id="new_unit_of_measure">
+                                    <option value=""></option>
+                                </select>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3"><div class='form-group'><label for="save">Actions</label><br /><button type='button' class='btn btn-success' name='save' onClick='createBM();'>Save</button></div></div>
+                    </div>
+                    </form>
+
+                </div>
                 <div id="building_materials_list">
                     <div class="panel">
                         <div class="panel-body">
@@ -140,6 +294,7 @@
                 <div class='modal-header'>
                     <button type='button' class='close' data-dismiss='modal'>&times;</button>
                     <h4 class='modal-title'>Tags</h4>
+
                 </div>
                 <div class='modal-body'>
                     <form id="tagForm">
@@ -152,6 +307,7 @@
             </div>
         </div>
     </div>
+
 @endsection
 
 
